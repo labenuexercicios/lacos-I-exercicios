1baa1d3ba0a61346222dfce3907c3546709fe1ab
Jessica Faustino <jessicacfaustino@gmail.com>
2022-08-16 18:54:54 -0300

    Finalizando exercicio 3

---
+++ exercicio3/script.js
@@ -0,0 +1,7 @@
+const maioresTimes = ["Campinense Clube", "Treze Futebol Clube", "Nacional de Patos", "Souza", "Botafogo"];
+let cont = 0;
+
+while (cont < maioresTimes.length){
+    console.log (`${cont+1} - ${maioresTimes[cont]}`);
+    cont = cont+1;
+}
