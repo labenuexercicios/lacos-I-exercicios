a5bc5e55452e5f2f2589653f4926497c7dfc6e4b
Julian Diel <52138612+JulianDiel@users.noreply.github.com>
2022-08-16 18:30:30 -0300

    xD

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,10 +1,10 @@
 
-let dadoInput = Number(prompt("Você quer comer mais coxinhas? \n 1 - Sim \n 0 - Não"))
-let somar = 2.50
+let dadoInput = (prompt("Você quer comer mais coxinhas? S / N, cada coxinha custa 2,50"))
+let somar = 0
 
-while(dadoInput !== 0){
-    somar += dadoInput
-    dadoInput = Number(prompt("Você quer comer mais coxinhas? \n 1 - Sim \n 0 - Não"))
+while(dadoInput.toUpperCase()==="S"){
+    somar = somar + 2.50
+    dadoInput = (prompt("Você quer comer mais coxinhas? S / N, cada coxinha custa 2,50"))
 
 }
 console.log(somar);
