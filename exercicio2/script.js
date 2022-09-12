6dda52ed069a72c9bac40d454172dd0a3fc1d655
guirozmesquita@gmail.com <guilhermesquita>
2022-09-12 11:51:30 -0400

    exercicio finalizado

---
+++ exercicio2/script.js
@@ -0,0 +1,4 @@
+let numberUser = prompt('Insira um número para a tabuada!');
+for(i=1; i<11; i++){
+    console.log(numberUser*i);
+}
---
+++ exercicio3/script.js
@@ -0,0 +1,4 @@
+let arrStr = ['Tlou', 'Uncharted', 'Red Dead II', 'Skyrim', 'LOZ']
+for(i = 0; i<arrStr.length; i++){
+    console.log(`${i+1} - ${arrStr[i]}`)
+}
