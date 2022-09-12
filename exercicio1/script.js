553f98fc2a36af40f0afb36a80f539aa9239634c
MailsonAndradeCardoso <mailsoncardoso1988@gmail.com>
2022-09-12 15:41:30 -0300

    Exercicio ok

---
+++ exercicio1/script.js
@@ -0,0 +1,20 @@
+/*# Exercício 1
+
+Utilizando o laço `while`, escreva um código que recebe um `prompt` perguntando se a pessoa deseja comer mais coxinhas. “S” deve representar “sim”, e “N” deve representar “não”. Crie também uma `let conta` para guardar o valor total.
+
+Toda vez que a resposta for **sim**, o laço deve se repetir, e devemos adicionar R$2.50 ao valor total. Quando a resposta não for **sim**, devemos imprimir o valor total da conta.*/
+
+let conta = 0
+const precoCoxinha = 2.5
+let maisCoxinha = prompt("quer mais coxinhas? S ou N")
+
+while(maisCoxinha !== "N"){
+    if(maisCoxinha === "S"){
+        coxinha = prompt("quer mais coxinhas? S ou N")
+        conta = conta + precoCoxinha
+    } else{
+        alert("resposta invalida!")
+        maisCoxinha = prompt("quer mais coxinhas? S ou N")}
+}
+
+console.log("Sua conta deu", conta)
