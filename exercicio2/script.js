79189ff69a0837f5384d489c9a68656b86b16892
Vanessa Farias <vr.farias1992@gmail.com>
2022-09-12 20:29:09 -0300

    exercícios sobre Laços no JS

---
+++ exercicio2/script.js
@@ -0,0 +1,6 @@
+let numero = Number(prompt("Qual tabuada você quer?"));
+
+for(let i = 1 ; i <= 10; i++) {
+    
+    console.log(`${numero} x ${i} = ${numero * i}`)
+};
---
+++ exercicio3/script.js
@@ -0,0 +1,7 @@
+const maioresPaises = ['Rússia', 'Canadá', 'China', 'EUA', 'Brasil'];
+
+for(let i = 0; i < maioresPaises.length; i++){
+    const ranking = maioresPaises[i]
+    console.log(i + 1 + " - " + ranking)
+};
+
