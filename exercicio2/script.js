59b48e06d3e304239c424a230c2401d9d024f9e5
Mjfreedom <mbuzina@gmail.com>
2022-09-12 12:53:25 -0300

    Exercicio 2 antes da aula

---
+++ exercicio2/script.js
@@ -0,0 +1,11 @@
+let inputUsuario = Number(prompt(`Digite um número qualquer:`))
+
+for (let i = 1; i <= 10; i++){
+    multiplicacao = i * inputUsuario
+    console.log(multiplicacao)
+}
+
+
+
+
+//imprimir a tabua de 1 a 10
