94832739707d7016197164cedda50fed9323387c
Amanda <amanda.digital@hotmail.com>
2023-09-20 19:31:00 -0300

    finalizar exercicios de fixação laços I

---
+++ exercicio1/script.js
@@ -0,0 +1,16 @@
+/*Utilizando o laço `while`, escreva um código que recebe um `prompt` perguntando se a 
+pessoa deseja comer mais coxinhas. “S” deve representar “sim”, e “N” deve representar “não”. Crie também
+ uma `let conta` para guardar o valor total.
+
+Toda vez que a resposta for **sim**, o laço deve se repetir, e devemos adicionar R$2.50 ao
+ valor total. Quando a resposta não for **sim**, devemos imprimir o valor total da conta.*/
+
+let coxinha = prompt ('você deseja comer mais coxinhas?').toLowerCase()
+ let conta = 0 
+
+  while ( coxinha === 's' ){
+  
+     conta = conta + 2.50
+     coxinha = prompt ('você deseja comer mais coxinhas? ').toLowerCase()  
+  }
+    console.log (`o valo da sua conta é ${conta}`)
