a4392adca7a45a8d196bac155f2eaba9bf4ff2e6
raissanfg <99144281+raissanfg@users.noreply.github.com>
2023-09-20 12:57:34 -0500

    exercicio-3-feito

---
+++ exercicio3/script.js
@@ -0,0 +1,11 @@
+function animais () {
+    const meuArray = ["cachorro", "gato", "cavalo", "passarinho", "baleia"]
+
+for ( let i = 0; i < meuArray.length; i++){
+    console.log(`${i + 1} - ${meuArray[i]}`)
+   
+
+}
+
+}
+animais()
